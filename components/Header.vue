a722fefbc443ce08fc70d3d2bcc86de9229f4b27
deadka1313 <deadka1313@gmail.com>
2018-08-24 20:12:50 +0700

    Добавил лого и начал оформлять header

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,18 @@
 <template>
 	<div class="container">
-		Header
+		<div class="header">
+			<nuxt-link to="/">
+				<Logo/>
+			</nuxt-link>
+		</div>
 	</div>
 </template>
+
+<script>
+import Logo from '~/components/Logo.vue'
+export default {
+	components: {
+		Logo
+	}
+}
+</script>
